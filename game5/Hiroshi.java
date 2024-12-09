fdbe8ecdebad0a24a1fc55f77a2163aa8a91ad16
菅原 拓実 <20088@hakodate.kosen-ac.jp>
2024-12-09 13:36:22 +0900

    JyoniとHirosiの移動するときの向きと名前を変えたよ！！

--- game5/Hiroshi.java
+++ game5/Hiroshi.java
@@ -6,20 +6,34 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class ひろし extends Actor
+public class Hiroshi extends Actor
 {
     /**
      * Act - do whatever the ひろし wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public ひろし(){
+    public Hiroshi(){
         getImage().scale(80,130);
     }
     public void act() 
     {
-        
+        int x = getX();
+        int y = getY();
+        int move = 4;
+        if( Greenfoot.isKeyDown( "a" ) ){
+            setLocation( x-move,y );
+        }
+        if( Greenfoot.isKeyDown( "d" ) ){
+            setLocation( x+move,y );
+        }
+        if( Greenfoot.isKeyDown( "w" ) ){
+            setLocation( x,y-move );
+        }
+        if( Greenfoot.isKeyDown( "s" ) ){
+            setLocation( x,y+move );
+        }
         // Add your action code here.
-        if( Greenfoot.isKeyDown( "d" ) )
+        /*if( Greenfoot.isKeyDown( "d" ) )
         {
             setRotation(0);
             move(4);
@@ -38,6 +52,6 @@ public class ひろし extends Actor
         {
             setRotation(90);
             move(4);
-        }
+        }*/
     }    
 }
--- game5/Jyoni.java
+++ game5/Jyoni.java
@@ -6,19 +6,34 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class ジョニー extends Actor
+public class Jyoni extends Actor
 {
     /**
-     * Act - do whatever the ジョニー wants to do. This method is called whenever
+     * Act - do whatever the Jyoni wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public ジョニー(){
+    public Jyoni(){
         getImage().scale(80,130);
     }
     public void act() 
     {
+        int x = getX();
+        int y = getY();
+        int move=4;
+        if( Greenfoot.isKeyDown( "left" ) ){
+            setLocation( x-move,y );
+        }
+        if( Greenfoot.isKeyDown( "right" ) ){
+            setLocation( x+move,y );
+        }
+        if( Greenfoot.isKeyDown( "up" ) ){
+            setLocation( x,y-move );
+        }
+        if( Greenfoot.isKeyDown( "down" ) ){
+            setLocation( x,y+move );
+        }
         // Add your action code here.
-        if( Greenfoot.isKeyDown( "right" ) )
+        /*if( Greenfoot.isKeyDown( "right" ) )
         {
             setRotation(0);
             move(4);
@@ -37,6 +52,6 @@ public class ジョニー extends Actor
         {
             setRotation(90);
             move(4);
-        }
+        }*/
     }    
 }
